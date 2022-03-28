02271077e1f5f8a6f8d724799219192772f21850
iamIsaacMu <iamisaacmu@gmail.com>
2022-03-28 22:51:10 +0700

    adds

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -1,3 +1,4 @@
+import { countReset } from 'console';
 import { EventEmitter } from './EventEmitter';
 
 /*
@@ -8,8 +9,15 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    doCount() {
+        obj.count++;
+    },
+    subscribe() {
+        EventEmitter.on('click', this.doCount);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', this.doCount);
+    },
 };
 
 /*
@@ -19,7 +27,9 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        this.second(...args.reverse());
+    },
     second() {
         // здесь ничего писать не нужно
     },
